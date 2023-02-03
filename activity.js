0cfdbc3c318a95abf82033f17fd8bc854c1338da
jkujo20 <jkujo20@yahoo.com>
2023-02-03 19:44:18 +0800

    First commit

---
+++ activity.js
@@ -0,0 +1,23 @@
+function swapNumbers(obj) {
+  obj.x += obj.y;
+
+  //there is a bug here
+  obj.y = obj.x + obj.y;
+
+  obj.x = obj.x - obj.y;
+}
+
+let swapObj = {
+  x: 10,
+  y: 11,
+};
+
+console.log("--------before swap------");
+console.log("x = " + swapObj.x);
+console.log("y = " + swapObj.y);
+
+swapNumbers(swapObj);
+
+console.log("--------after swap------");
+console.log("x = " + swapObj.x);
+console.log("y = " + swapObj.y);
